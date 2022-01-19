ea89597f8ca381200886dce4ececf93b88b785aa
Shene rashid <shene.rashid999@gmail.com>
2022-01-19 22:36:46 +0300

    this is the rest.dart

--- lib/services/rest.dart
+++ lib/services/rest.dart
@@ -8,10 +8,8 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class Rest {
-  //? Change the baseUrl according to your PC's IP address. Remain the port as 3000
-  static const String _baseUrl = 'http://192.168.0.105:3000';
+  static const String _baseUrl = '';
 
-  // Send a GET request to retrieve data from a REST server
   static Future get(String endpoint) async {
     final response = await http.get(Uri.parse('$_baseUrl/$endpoint'));
 
@@ -21,18 +19,16 @@ class Rest {
     throw response;
   }
 
-  // Send a POST request to add a new in the REST server
   static Future post(String endpoint, {dynamic data}) async {
     final response = await http.post(Uri.parse('$_baseUrl/$endpoint'),
         headers: {'Content-Type': 'application/json'}, body: jsonEncode(data));
 
-    if (response.statusCode == 201) {
+    if (response.statusCode == 200) {
       return jsonDecode(response.body);
     }
     throw response;
   }
 
-  // Send a PUT request to update an existing data in the REST server.
   static Future put(String endpoint, {dynamic data}) async {
     final response = await http.put(Uri.parse('$_baseUrl/$endpoint'),
         headers: {'Content-Type': 'application/json'}, body: jsonEncode(data));
@@ -43,7 +39,6 @@ class Rest {
     throw response;
   }
 
-  // Send a PATCH request to update parts of an existing data in the REST server.
   static Future patch(String endpoint, {dynamic data}) async {
     final response = await http.patch(Uri.parse('$_baseUrl/$endpoint'),
         headers: {'Content-Type': 'application/json'}, body: jsonEncode(data));
@@ -54,7 +49,6 @@ class Rest {
     throw response;
   }
 
-  // Send a DELETE request to remove an existing data from the REST server.
   static Future delete(String endpoint) async {
     final response = await http.delete(Uri.parse('$_baseUrl/$endpoint'));
 
