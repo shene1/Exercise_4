27c85f495c98bb02afdfffc10322c03a28061e56
Shene rashid <shene.rashid999@gmail.com>
2022-01-19 20:37:05 +0300

    this is the part of body.dart for edit folder

---
+++ lib/screens/edit/body.dart
@@ -0,0 +1,94 @@
+// TODO Complete the file, edit/body.dart
+//-----------------------------------------------------------------------------------------------------------------------------
+//? Things to do:
+//   1. Build this screen with conditional UI technique (based on
+//      whether it is for 'editing an existing todo' or 'creating a new todo').
+//      This includes:
+//        a. The 'done' field will only be enabled if it is an 'editing' mode
+//
+//   2. Update the states and reflect the changes on the UI:
+//        a. when the user types in the 'title'.
+//        b. when the user types in the 'description'.
+//        c. when the user taps on the 'done' checkbox.
+//
+//   3. Perform the following operations:
+//        a. Accept the updated / created item - i.e. when the 'Ok' button is tapped on.
+//        b. Cancel the changes - i.e. when the 'Cancel' button is tapped on.
+//-----------------------------------------------------------------------------------------------------------------------------
+
+import 'package:exerciseee/models/todo.dart';
+import 'package:flutter/material.dart';
+
+import 'edit_screen.dart';
+
+class Body extends StatelessWidget {
+  const Body({state}) : _state = state;
+
+  final EditScreenState _state;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: [
+        ListTile(
+          title: TextFormField(
+            initialValue:
+                _state.widget.isEditing ? _state.widget.data.title : '',
+            decoration: InputDecoration(
+              labelText: 'Title',
+            ),
+            onChanged: (valuee) => {_state.updateTitle, print("HI")},
+          ),
+        ),
+        _buildTextLisTile(
+            label: 'Description',
+            value:
+                _state.widget.isEditing ? _state.widget.data.description : '',
+            onChanged: (value) {
+              _state.todo.description = value;
+            }),
+        _state.widget.isEditing
+            ? CheckboxListTile(
+                value: _state.todo.done,
+                onChanged: _state.updateDone,
+                title: Text('Done'),
+              )
+            : Text(""),
+        _buildButtons(context)
+      ],
+    );
+  }
+
+  ListTile _buildTextLisTile({label, value, onChanged}) {
+    return ListTile(
+      title: TextFormField(
+        initialValue: value,
+        decoration: InputDecoration(
+          labelText: label,
+        ),
+        onChanged: onChanged,
+      ),
+    );
+  }
+
+  Row _buildButtons(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        ElevatedButton(
+          child: Text('Ok'),
+          onPressed: () {
+            Navigator.of(context).pop(_state.widget.data);
+          },
+        ),
+        SizedBox(width: 10.0),
+        ElevatedButton(
+          child: Text('Cancel'),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
+      ],
+    );
+  }
+}
