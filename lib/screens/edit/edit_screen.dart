28bf4cf58ebc27ae4dfe7528de8de000851a9e54
Shene rashid <shene.rashid999@gmail.com>
2022-01-19 20:29:19 +0300

    this the part of edit_screen.dart

---
+++ lib/screens/edit/edit_screen.dart
@@ -0,0 +1,64 @@
+// TODO Complete the file edit_screen.dart
+//-----------------------------------------------------------------------------------------------------------------------------
+//? Things to do:
+//
+//   1. Build the UI by composing from its components, i.e., Bar and Body.
+//      Besides, you will also need to pass the 'states' to the components.
+//-----------------------------------------------------------------------------------------------------------------------------
+
+import 'package:exerciseee/screens/edit/bar.dart';
+import 'package:flutter/material.dart';
+
+import '../../models/todo.dart';
+import 'body.dart';
+
+class EditScreen extends StatefulWidget {
+  static Route route({isEditing = true, data}) => MaterialPageRoute(
+      builder: (context) => EditScreen(isEditing: isEditing, data: data));
+
+  final bool _isEditing;
+  final Todo _data;
+
+  get isEditing => _isEditing;
+
+  get data => _data;
+  EditScreen({isEditing, data})
+      : _isEditing = isEditing,
+        _data = data;
+
+  @override
+  EditScreenState createState() => EditScreenState(_data);
+}
+
+class EditScreenState extends State<EditScreen> {
+  Todo todo;
+  EditScreenState(this.todo);
+  void updateDone(bool value) {
+    setState(() {
+      todo.done = value;
+    });
+  }
+
+  void updateTitle(String value) {
+    setState(() {
+      todo.title = value;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+      onWillPop: () => Future.value(false),
+      child: SafeArea(
+        child: Scaffold(
+          appBar: Bar(
+            state: this,
+          ),
+          body: Body(
+            state: this,
+          ),
+        ),
+      ),
+    );
+  }
+}
