4cde803a1f8eaf8f5c3ee6a78543a0b8cbcbe698
Shene rashid <shene.rashid999@gmail.com>
2022-01-19 22:19:41 +0300

    this is user_srvice.dart

---
+++ lib/services/user_service.dart
@@ -0,0 +1,23 @@
+// TODO Complete the service class, UserService
+//-----------------------------------------------------------------------------------------------------------------------------
+//? Things to do:
+//   Define the following method to handle REST call about the User data
+//    to the REST service.
+//       a. getUserByLoginAndPassword: to get a user data, given his/her login and password.
+//          If no match, this method returns null.
+//          This method will be used for constructing the logic of the authentication process in the LoginScreen
+//-----------------------------------------------------------------------------------------------------------------------------
+
+import 'rest.dart';
+import '../models/user.dart';
+
+class UserService {
+  static Future<User> getUserByLoginAndPassword(
+      {String login, String password}) async {
+    final user = await Rest.get('users?login=$login&password=$password');
+    if (user.length != 0) {
+      return User.fromJson(user[0]);
+    }
+    return null;
+  }
+}
